35ba68a5d0338b401c1a86e70bf83b4f9531508e
Riley Bakes <48607876+r-bakes@users.noreply.github.com>
2026-04-18 16:41:44 -0400

    bug(python-lsp): set basedpyright as python lsp until nvim 0.12 is stable. refactor rename functionality broken.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 💤 LazyVim
 
+> Clone me into `$HOME/.config` and you should be good to go!
+
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- NOTE: nvim nightly .12 unstable fix for refactor not working in python. Use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
